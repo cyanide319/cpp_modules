5beec4bc9871536ad213b8ed58bcc58d1c281211
cyanide319 <tristan-b@live.ca>
2023-03-17 14:55:53 -0400

    ctrl+D

--- mod_00/ex01/PhoneBook.cpp
+++ mod_00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: tbeaudoi <tbeaudoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 14:06:50 by tbeaudoi          #+#    #+#             */
-/*   Updated: 2023/03/17 14:44:55 by tbeaudoi         ###   ########.fr       */
+/*   Updated: 2023/03/17 14:55:12 by tbeaudoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,38 +24,53 @@ int	PhoneBook::add_contact(){
 	
 	std::cout << "Enter first name:";
 	getline(std::cin, str);
-	if (str.size() <= 0)
-		return (-1);
-	str = PhoneBook::format_str(str);
-	contacts[PhoneBook::_index].set_first_name(str);
+	if (std::cin.good()){
+		if (str.size() <= 0)
+			return (-1);
+		str = PhoneBook::format_str(str);
+		contacts[PhoneBook::_index].set_first_name(str);
+	}
+	else exit(0);
 
 	std::cout << "Enter last name:";
 	getline(std::cin, str);
-	if (str.size() <= 0)
-		return (-1);
-	str = PhoneBook::format_str(str);
-	contacts[PhoneBook::_index].set_last_name(str);
+	if (std::cin.good()){
+		if (str.size() <= 0)
+			return (-1);
+		str = PhoneBook::format_str(str);
+		contacts[PhoneBook::_index].set_last_name(str);
+	}
+	else exit(0);
 	
 	std::cout << "Enter nickname:";
 	getline(std::cin, str);
-	if (str.size() <= 0)
-		return (-1);
-	str = PhoneBook::format_str(str);
-	contacts[PhoneBook::_index].set_nickname(str);
+	if (std::cin.good()){
+		if (str.size() <= 0)
+			return (-1);
+		str = PhoneBook::format_str(str);
+		contacts[PhoneBook::_index].set_nickname(str);
+	}
+	else exit(0);
 	
 	std::cout << "Enter phone number:";
 	getline(std::cin, str);
-	if (str.size() <= 0)
-		return (-1);
-	str = PhoneBook::format_str(str);
-	contacts[PhoneBook::_index].set_phone_number(str);
+	if (std::cin.good()){
+		if (str.size() <= 0)
+			return (-1);
+		str = PhoneBook::format_str(str);
+		contacts[PhoneBook::_index].set_phone_number(str);
+	}
+	else exit(0);
 	
 	std::cout << "Enter dark secret:";
 	getline(std::cin, str);
-	if (str.size() <= 0)
-		return (-1);
-	str = PhoneBook::format_str(str);
-	contacts[PhoneBook::_index].set_dark_secret(str);
+	if (std::cin.good()){
+		if (str.size() <= 0)
+			return (-1);
+		str = PhoneBook::format_str(str);
+		contacts[PhoneBook::_index].set_dark_secret(str);
+	}
+	else exit(0);
 
 	PhoneBook::_swap_flag = 0;
 	if (PhoneBook::_index < 7)
@@ -93,64 +108,66 @@ int	PhoneBook::search_contact(){
 	}
 	std::cout << "Enter index:";
 	getline(std::cin, input);
-	if(input == "0"){
-		std::cout << "First name: " << contacts[0].get_first_name() << std::endl
-		<< "Last name: " << contacts[0].get_last_name() << std::endl
-		<< "Nickname: " << contacts[0].get_nickname() << std::endl
-		<< "Phone number: " << contacts[0].get_phone_number() << std::endl
-		<< "Dark secret: " << contacts[0].get_dark_secret() << std::endl;
+	if (std::cin.good()){
+		if(input == "0"){
+			std::cout << "First name: " << contacts[0].get_first_name() << std::endl
+			<< "Last name: " << contacts[0].get_last_name() << std::endl
+			<< "Nickname: " << contacts[0].get_nickname() << std::endl
+			<< "Phone number: " << contacts[0].get_phone_number() << std::endl
+			<< "Dark secret: " << contacts[0].get_dark_secret() << std::endl;
+		}
+		else if(input == "1"){
+			std::cout << "First name: " << contacts[1].get_first_name() << std::endl
+			<< "Last name: " << contacts[1].get_last_name() << std::endl
+			<< "Nickname: " << contacts[1].get_nickname() << std::endl
+			<< "Phone number: " << contacts[1].get_phone_number() << std::endl
+			<< "Dark secret: " << contacts[1].get_dark_secret() << std::endl;
+		}
+		else if(input == "2"){
+			std::cout << "First name: " << contacts[2].get_first_name() << std::endl
+			<< "Last name: " << contacts[2].get_last_name() << std::endl
+			<< "Nickname: " << contacts[2].get_nickname() << std::endl
+			<< "Phone number: " << contacts[2].get_phone_number() << std::endl
+			<< "Dark secret: " << contacts[2].get_dark_secret() << std::endl;
+		}
+		else if(input == "3"){
+			std::cout << "First name: " << contacts[3].get_first_name() << std::endl
+			<< "Last name: " << contacts[3].get_last_name() << std::endl
+			<< "Nickname: " << contacts[3].get_nickname() << std::endl
+			<< "Phone number: " << contacts[3].get_phone_number() << std::endl
+			<< "Dark secret: " << contacts[3].get_dark_secret() << std::endl;
+		}
+		else if(input == "4"){
+			std::cout << "First name: " << contacts[4].get_first_name() << std::endl
+			<< "Last name: " << contacts[4].get_last_name() << std::endl
+			<< "Nickname: " << contacts[4].get_nickname() << std::endl
+			<< "Phone number: " << contacts[4].get_phone_number() << std::endl
+			<< "Dark secret: " << contacts[4].get_dark_secret() << std::endl;
+		}
+		else if(input == "5"){
+			std::cout << "First name: " << contacts[5].get_first_name() << std::endl
+			<< "Last name: " << contacts[5].get_last_name() << std::endl
+			<< "Nickname: " << contacts[5].get_nickname() << std::endl
+			<< "Phone number: " << contacts[5].get_phone_number() << std::endl
+			<< "Dark secret: " << contacts[5].get_dark_secret() << std::endl;
+		}
+		else if(input == "6"){
+			std::cout << "First name: " << contacts[6].get_first_name() << std::endl
+			<< "Last name: " << contacts[6].get_last_name() << std::endl
+			<< "Nickname: " << contacts[6].get_nickname() << std::endl
+			<< "Phone number: " << contacts[6].get_phone_number() << std::endl
+			<< "Dark secret: " << contacts[6].get_dark_secret() << std::endl;
+		}
+		else if(input == "7"){
+			std::cout << "First name: " << contacts[7].get_first_name() << std::endl
+			<< "Last name: " << contacts[7].get_last_name() << std::endl
+			<< "Nickname: " << contacts[7].get_nickname() << std::endl
+			<< "Phone number: " << contacts[7].get_phone_number() << std::endl
+			<< "Dark secret: " << contacts[7].get_dark_secret() << std::endl;
+		}
+		else
+			std::cout << input << " is not a valid index." << std::endl;
 	}
-	else if(input == "1"){
-		std::cout << "First name: " << contacts[1].get_first_name() << std::endl
-		<< "Last name: " << contacts[1].get_last_name() << std::endl
-		<< "Nickname: " << contacts[1].get_nickname() << std::endl
-		<< "Phone number: " << contacts[1].get_phone_number() << std::endl
-		<< "Dark secret: " << contacts[1].get_dark_secret() << std::endl;
-	}
-	else if(input == "2"){
-		std::cout << "First name: " << contacts[2].get_first_name() << std::endl
-		<< "Last name: " << contacts[2].get_last_name() << std::endl
-		<< "Nickname: " << contacts[2].get_nickname() << std::endl
-		<< "Phone number: " << contacts[2].get_phone_number() << std::endl
-		<< "Dark secret: " << contacts[2].get_dark_secret() << std::endl;
-	}
-	else if(input == "3"){
-		std::cout << "First name: " << contacts[3].get_first_name() << std::endl
-		<< "Last name: " << contacts[3].get_last_name() << std::endl
-		<< "Nickname: " << contacts[3].get_nickname() << std::endl
-		<< "Phone number: " << contacts[3].get_phone_number() << std::endl
-		<< "Dark secret: " << contacts[3].get_dark_secret() << std::endl;
-	}
-	else if(input == "4"){
-		std::cout << "First name: " << contacts[4].get_first_name() << std::endl
-		<< "Last name: " << contacts[4].get_last_name() << std::endl
-		<< "Nickname: " << contacts[4].get_nickname() << std::endl
-		<< "Phone number: " << contacts[4].get_phone_number() << std::endl
-		<< "Dark secret: " << contacts[4].get_dark_secret() << std::endl;
-	}
-	else if(input == "5"){
-		std::cout << "First name: " << contacts[5].get_first_name() << std::endl
-		<< "Last name: " << contacts[5].get_last_name() << std::endl
-		<< "Nickname: " << contacts[5].get_nickname() << std::endl
-		<< "Phone number: " << contacts[5].get_phone_number() << std::endl
-		<< "Dark secret: " << contacts[5].get_dark_secret() << std::endl;
-	}
-	else if(input == "6"){
-		std::cout << "First name: " << contacts[6].get_first_name() << std::endl
-		<< "Last name: " << contacts[6].get_last_name() << std::endl
-		<< "Nickname: " << contacts[6].get_nickname() << std::endl
-		<< "Phone number: " << contacts[6].get_phone_number() << std::endl
-		<< "Dark secret: " << contacts[6].get_dark_secret() << std::endl;
-	}
-	else if(input == "7"){
-		std::cout << "First name: " << contacts[7].get_first_name() << std::endl
-		<< "Last name: " << contacts[7].get_last_name() << std::endl
-		<< "Nickname: " << contacts[7].get_nickname() << std::endl
-		<< "Phone number: " << contacts[7].get_phone_number() << std::endl
-		<< "Dark secret: " << contacts[7].get_dark_secret() << std::endl;
-	}
-	else
-		std::cout << input << " is not a valid index." << std::endl;
 	return (0);
 }
 
