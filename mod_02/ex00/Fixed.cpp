8deaa26d0380c4f9a898fe2b73e021b6faeb7963
cyanide319 <tristan-b@live.ca>
2023-03-31 13:12:01 -0400

    ex00 finished, I think

--- mod_02/ex00/Fixed.cpp
+++ mod_02/ex00/Fixed.cpp
@@ -1,17 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbeaudoi <tbeaudoi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/31 12:47:06 by tbeaudoi          #+#    #+#             */
+/*   Updated: 2023/03/31 13:09:56 by tbeaudoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
 #include "Fixed.hpp"
 
 Fixed::Fixed(): _fixed_point(0){
     std::cout << "Default construtor called" << std::endl;
 }
 
-Fixed::Fixed(const Fixed& new_object){
+Fixed::Fixed(const Fixed& other){
     std::cout << "Copy construtor called" << std::endl;
-    this->_fixed_point = new_object._fixed_point;
+    *this = other;
 }
 
-Fixed::~Fixed(){}
+Fixed::~Fixed(){
+    std::cout << "Destructor called" << std::endl;
+}
 
 int Fixed::getRawBits(void) const{
+    std::cout << "getRawBits member function called" << std::endl;
     return (_fixed_point);
 }
 
@@ -19,6 +35,9 @@ void    Fixed::setRawBits(int const new_val){
     Fixed::_fixed_point = new_val;
 }
 
-Fixed& Fixed::operator=(const Fixed& ohter){
-    
+Fixed& Fixed::operator=(const Fixed& other){
+    std::cout << "Copy assignment operator called" << std::endl;
+    if (this != &other)
+        _fixed_point = other.getRawBits();
+    return (*this);
 }
---
+++ mod_02/ex00/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbeaudoi <tbeaudoi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/31 12:47:26 by tbeaudoi          #+#    #+#             */
+/*   Updated: 2023/03/31 12:51:00 by tbeaudoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int	main(void){
+Fixed a;
+Fixed b( a );
+Fixed c;
+
+c = b;
+
+std::cout << a.getRawBits() << std::endl;
+std::cout << b.getRawBits() << std::endl;
+std::cout << c.getRawBits() << std::endl;
+
+return 0;
+}
