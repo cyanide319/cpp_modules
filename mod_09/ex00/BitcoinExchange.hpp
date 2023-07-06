89cb31c130fe7481dfb45b6eb451ae4cc1025f73
cyanide319 <tristan-b@live.ca>
2023-07-06 15:57:03 -0400

    begin of ex00/mod09

---
+++ mod_09/ex00/BitcoinExchange.hpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tbeaudoi <tbeaudoi@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/06 15:13:07 by tbeaudoi          #+#    #+#             */
+/*   Updated: 2023/07/06 15:46:22 by tbeaudoi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef BITCOINEXCHANGE_HPP
+#define BITCOINEXCHANGE_HPP
+
+#include <map>
+
+class BitcoinExchange{
+	public:
+
+	private:
+		std::map<int[3], float>	_data;
+};
+
+#endif
