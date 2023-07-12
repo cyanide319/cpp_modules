1ad288628b8da1a3dfaf9a4a898d15ae882fc714
cyanide319 <tristan-b@live.ca>
2023-07-12 17:19:01 -0400

    RPN segfault fixed

--- mod_09/ex01/RPN.cpp
+++ mod_09/ex01/RPN.cpp
@@ -6,7 +6,7 @@
 /*   By: tbeaudoi <tbeaudoi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 15:02:00 by tbeaudoi          #+#    #+#             */
-/*   Updated: 2023/07/12 16:27:54 by tbeaudoi         ###   ########.fr       */
+/*   Updated: 2023/07/12 16:55:45 by tbeaudoi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,32 +43,38 @@ int	RPN::parse_input(std::string input){
 		throw std::runtime_error("Error: invalid character.");
 	std::stringstream 			ss(input);
 	std::string 				token;
-	std::string*				array = NULL;
 	int							i = 0;
 
 	while(std::getline(ss, token, ' ')){
+		i++;
+	}
+	std::string*	array = new std::string[i];
+	i = 0;
+	std::stringstream 			ss2(input);
+	while(std::getline(ss2, token, ' ')){
 		array[i] += token;
 		i++;
 	}
 	if (i % 2 == 0)
 		throw std::runtime_error("Error: wrong number of argument. The sum of digits and operand should always be odd");
-	if (is_string_digit(array[0]) == false || is_string_digit(array[1]) == false)
-		throw std::runtime_error("Error: values must be numbers between 0 and 9");
+	if (is_string_digit(array[0]) == false)
+		throw std::runtime_error("Error: values must be numbers between 0 and 9 1");
 
 	std::istringstream value1(array[0]);
 	int temp;
 	value1 >> temp;
 	if (temp < 1 || temp > 9)
-		throw std::runtime_error("Error: values must be numbers between 0 and 9");
+		throw std::runtime_error("Error: values must be numbers between 0 and 9 2");
 	_stack.push(temp);
 
 
 	for(int j = 1; j < i; j++){
 		int result = 0;
+		std::cout << array[j] << std::endl;
 		std::istringstream value2(array[j]);
 		value2 >> temp;
 		if (temp < 1 || temp > 9)
-			throw std::runtime_error("Error: values must be numbers between 0 and 9");
+			throw std::runtime_error("Error: values must be numbers between 0 and 9 3");
 		if (array[j + 1] == "+")
 			result = temp + _stack.top();
 		else if (array[j + 1] == "-")
@@ -81,7 +87,7 @@ int	RPN::parse_input(std::string input){
 			throw std::runtime_error("Error: expected a operations token");
 		_stack.pop();
 		_stack.push(result);
-		j += 2;		
+		j += 1;		
 	}
 	std::cout << _stack.top() << std::endl;
 	return (0);
